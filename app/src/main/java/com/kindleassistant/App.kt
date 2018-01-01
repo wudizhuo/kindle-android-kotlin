9c9208c488b76caaa31330c935bafc0bfcb65c06
wudizhuo <sunzhuo1228@126.com>
2018-01-01 13:30:26 -0800

    add Fabric

---
+++ app/src/main/java/com/kindleassistant/App.kt
@@ -0,0 +1,12 @@
+package com.kindleassistant
+
+import android.app.Application
+import com.crashlytics.android.Crashlytics
+import io.fabric.sdk.android.Fabric
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Fabric.with(this, Crashlytics())
+    }
+}
