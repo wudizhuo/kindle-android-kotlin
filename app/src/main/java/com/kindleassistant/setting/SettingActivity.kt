0e55ef28441d57a26e57eea77938e86dc6617bfe
wudizhuo <sunzhuo1228@126.com>
2018-01-07 21:02:10 -0800

    build version 1

--- app/src/main/java/com/kindleassistant/setting/SettingActivity.kt
+++ app/src/main/java/com/kindleassistant/setting/SettingActivity.kt
@@ -1,13 +1,12 @@
 package com.kindleassistant.setting
 
 import android.os.Bundle
+import android.support.design.widget.Snackbar
 import android.text.TextUtils
-import android.widget.Toast
 import com.kindleassistant.AppPreferences
 import com.kindleassistant.R
 import com.kindleassistant.common.BaseActivity
 import kotlinx.android.synthetic.main.activity_setting.*
-import java.util.*
 
 class SettingActivity : BaseActivity() {
 
@@ -15,39 +14,41 @@ class SettingActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_setting)
 
-        val email = AppPreferences.toEmail
-        if (!TextUtils.isEmpty(email)) {
-            val split = email.split("@".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
-            if (split.size == 2) {
-                et_user_email.setText(split[0])
-                val stringArray = resources.getStringArray(
-                        R.array.sp_emails)
-                val asList = Arrays.asList(*stringArray)
-                val position = asList.indexOf("@" + split[1])
-                sp_emails!!.setSelection(position, true)
-            }
+        val fromEmailPref = AppPreferences.fromEmail
+        if (TextUtils.isEmpty(fromEmailPref)) {
+            input_layout_from_email.isErrorEnabled = true
+            input_layout_from_email.error = getString(R.string.from_email_hint)
+        } else {
+            et_from_email.setText(fromEmailPref)
         }
 
-        val userFromEmail = AppPreferences.fromEmail
-        if (!TextUtils.isEmpty(userFromEmail)) {
-            et_from_email.setText(userFromEmail)
-        }
-
-        bt_save_useremail.setOnClickListener {
-            val userEmail = et_user_email.text.toString() + sp_emails!!.selectedItem.toString()
-            val userFromEmail = et_from_email.text.toString()
-
-            if (TextUtils.isEmpty(userEmail) || TextUtils.isEmpty(userFromEmail)) {
-                Toast.makeText(applicationContext, "请设置完整",
-                        Toast.LENGTH_SHORT).show()
-                return@setOnClickListener
-            }
-
-            AppPreferences.setEmail(userEmail)
-            AppPreferences.setFromEmail(userFromEmail)
-            Toast.makeText(applicationContext, "设置成功",
-                    Toast.LENGTH_SHORT).show()
+        val toEmailPref = AppPreferences.toEmail
+        if (TextUtils.isEmpty(toEmailPref)) {
+            input_layout_to_email.isErrorEnabled = true
+            input_layout_to_email.error = getString(R.string.to_email_hint)
+        } else {
+            val split = toEmailPref.split(Regex("@")).dropLastWhile { it.isEmpty() }.toTypedArray()
+            et_to_email.setText(split[0])
+            val position = resources.getStringArray(
+                    R.array.sp_emails).indexOf("@" + split[1])
+            sp_emails.setSelection(position, true)
         }
         webview.loadUrl("file:///android_asset/setting-guide.html")
+
+        bt_save.setOnClickListener {
+            val fromEmail = et_from_email.text.toString()
+            val toEmail = et_to_email.text.toString() + sp_emails.selectedItem.toString()
+
+            fromEmail.isNotEmpty().apply {
+                input_layout_from_email.isErrorEnabled = false
+            }
+            toEmail.isNotEmpty().apply {
+                input_layout_to_email.isErrorEnabled = false
+            }
+
+            AppPreferences.setEmail(toEmail)
+            AppPreferences.setFromEmail(fromEmail)
+            Snackbar.make(bt_save, R.string.show_done, Snackbar.LENGTH_LONG).show()
+        }
     }
 }
